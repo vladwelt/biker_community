75f6c5548e61d275e0a019243e5859ef59f64b5c
vladwelt <vladwelt@gmail.com>
2015-12-18 11:04:38 -0400

    [EDT] Registered models to administer.

--- src/biker/admin.py
+++ src/biker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
+
+admin.site.register(Grupo)
+admin.site.register(User)
+admin.site.register(Ruta)
+admin.site.register(Evento)
+
 # Register your models here.
