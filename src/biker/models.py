b54679bed5270d90780134c56cf5aed84351b3fd
vladwelt <vladwelt@gmail.com>
2015-12-18 12:53:11 -0400

    [EDT] Show events.

--- src/biker/models.py
+++ src/biker/models.py
@@ -3,16 +3,25 @@ from django.db import models
 class Grupo(models.Model):
     nombre = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nombre
+
 class User(models.Model):
     nombre = models.CharField(max_length=100)
     correo = models.EmailField(max_length=100)
     fecha_nacimiento = models.DateField()
     grupo = models.ForeignKey(Grupo)
 
+    def __str__(self):
+        return self.nombre
+
 class Ruta(models.Model):
     nombre = models.CharField(max_length=100)
     distancia = models.FloatField()
     usuario = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.nombre
 
 class Evento(models.Model):
     nombre = models.CharField(max_length=100)
@@ -21,4 +30,6 @@ class Evento(models.Model):
     ruta = models.ForeignKey(Ruta)
     grupo = models.ForeignKey(Grupo)
 
+    def __str__(self):
+        return self.nombre
 # Create your models here.
--- src/biker/views.py
+++ src/biker/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from .models import Evento
 
+def group_list(request):
+    eventos = Evento.objects.filter(fecha__lte=timezone.now()).order_by('fecha')
+    return render(request, 'biker/group_list.html',{'eventos':eventos})
+
+def event_detail(request, pk):
+    evento = get_object_or_404(Evento, pk=pk)
+    return render(request, 'biker/event_detail.html',{'evento': evento})
 # Create your views here.
