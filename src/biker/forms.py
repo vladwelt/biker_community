28b709e43d7ef6267b8a24e0a1c3c9b6d8b595f5
vladwelt <vladwelt@gmail.com>
2016-02-10 10:52:17 -0400

    [EDT] Some class and styles in forms.

--- src/biker/forms.py
+++ src/biker/forms.py
@@ -5,6 +5,10 @@ class EventoForm(forms.ModelForm):
     class Meta:
         model = Evento
         fields = ['nombre','punto_partida','fecha','ruta','grupo']
+    def __init__(self, *args, **kwargs):
+        super(EventoForm, self).__init__(*args, **kwargs)
+        self.fields['ruta'].widget.attrs.update({'class' : 'form-control'})
+        self.fields['grupo'].widget.attrs.update({'class' : 'form-control','multiple':''})
 
 class GrupoForm(forms.ModelForm):
     class Meta:
@@ -15,3 +19,7 @@ class RutaForm(forms.ModelForm):
     class Meta:
         model = Ruta
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(RutaForm, self).__init__(*args, **kwargs)
+        self.fields['usuario'].widget.attrs.update({'class' : 'form-control'})
