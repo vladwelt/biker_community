f34a5e05193e7f38538c58b0ea192ce5dd9cf877
vladwelt <vladwelt@gmail.com>
2016-03-02 11:29:26 -0400

    [FIX] Login_required decorator added in Create, Remove, Update.

--- src/biker/urls.py
+++ src/biker/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login, logout
 from django.conf.urls import include, url
 from django.conf import settings
@@ -7,21 +8,21 @@ from biker.views import *
 
 urlpatterns = [
         url(r'^$', views.index),
-        url(r'^login/$', login),
+        url(r'^accounts/login/$', login),
         url(r'^logout/$', logout),
-        url(r'route/add/$', RutaCreate.as_view(), name='route-add'),
-        url(r'route/delete/(?P<pk>[0-9]+)/$', RutaDelete.as_view(), name='route-delete'),
+        url(r'route/add/$', login_required(RutaCreate.as_view()), name='route-add'),
+        url(r'route/delete/(?P<pk>[0-9]+)/$', login_required(RutaDelete.as_view()), name='route-delete'),
         url(r'route/detail/(?P<pk>[0-9]+)/$', RutaDetail.as_view(), name='route-detail'),
-        url(r'route/update/(?P<pk>[0-9]+)/$', RutaUpdate.as_view(), name='route-update'),
+        url(r'route/update/(?P<pk>[0-9]+)/$', login_required(RutaUpdate.as_view()), name='route-update'),
         url(r'route/list/$', RutaListView.as_view(), name='ruta-list'),
-        url(r'group/add/$', GrupoCreate.as_view(), name='group-add'),
-        url(r'group/delete/(?P<pk>[0-9]+)/$', GrupoDelete.as_view(), name='group-delete'),
+        url(r'group/add/$', login_required(GrupoCreate.as_view()), name='group-add'),
+        url(r'group/delete/(?P<pk>[0-9]+)/$', login_required(GrupoDelete.as_view()), name='group-delete'),
         url(r'group/detail/(?P<pk>[0-9]+)/$', GrupoDetail.as_view(), name='group-detail'),
-        url(r'group/update/(?P<pk>[0-9]+)/$', GrupoUpdate.as_view(), name='group-update'),
+        url(r'group/update/(?P<pk>[0-9]+)/$', login_required(GrupoUpdate.as_view()), name='group-update'),
         url(r'group/list/$', GrupoListView.as_view(), name='group-list'),
-        url(r'event/add/$', EventoCreate.as_view(), name='event-add'),
-        url(r'event/delete/(?P<pk>[0-9]+)/$', EventoDelete.as_view(), name='event-delete'),
+        url(r'event/add/$', login_required(EventoCreate.as_view()), name='event-add'),
+        url(r'event/delete/(?P<pk>[0-9]+)/$', login_required(EventoDelete.as_view()), name='event-delete'),
         url(r'event/detail/(?P<pk>[0-9]+)/$', EventoDetail.as_view(), name='event-detail'),
-        url(r'event/update/(?P<pk>[0-9]+)/$', EventoUpdate.as_view(), name='event-update'),
+        url(r'event/update/(?P<pk>[0-9]+)/$', login_required(EventoUpdate.as_view()), name='event-update'),
         url(r'event/list/$', EventoListView.as_view(), name='event-list'),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
